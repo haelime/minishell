875c603237ce2a301709b95d0b4b48e27ed5ccb4
haeem <haeem@student.42seoul.kr>
2023-08-16 16:39:18 +0900

    get_input header fixed

--- srcs/parse/get_input.c
+++ srcs/parse/get_input.c
@@ -6,11 +6,13 @@
 /*   By: haeem <haeem@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 16:32:12 by haeem             #+#    #+#             */
-/*   Updated: 2023/08/16 16:32:33 by haeem            ###   ########seoul.kr  */
+/*   Updated: 2023/08/16 16:38:57 by haeem            ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+#include "../../include/hashlib.h"
+#include "../../libft/include/libft.h"
 
 void	get_input(char *input, t_hashmap *envmap)
 {
