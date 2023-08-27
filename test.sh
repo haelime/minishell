53617cf9cc33056aac53ce280d4aac467b7f4dde
haelime <gnostalgiclife@gmail.com>
2023-08-27 22:39:48 +0900

    action test

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
 git clone https://github.com/LucasKuhn/minishell_tester.git
 cd minishell_tester
 ./tester
+cd ..
+git clone https://github.com/solaldunckel/minishell-tester.git
+cd minishell-tester && bash test.sh
