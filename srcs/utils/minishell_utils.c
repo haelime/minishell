1bcb939689eb7f2360dfb583341240551b2d21cd
haelime <gnostalgiclife@gmail.com>
2023-09-21 17:31:57 +0900

    attribute added on msg_exit functions

--- srcs/utils/minishell_utils.c
+++ srcs/utils/minishell_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell_utils.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyunjunk <hyunjunk@student.42.fr>          +#+  +:+       +#+        */
+/*   By: haeem <haeem@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 19:24:09 by haeem             #+#    #+#             */
-/*   Updated: 2023/09/18 17:17:58 by hyunjunk         ###   ########.fr       */
+/*   Updated: 2023/09/20 18:06:27 by haeem            ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,13 +49,14 @@ bool	isdoublequoted(char *str, int i)
 	return (false);
 }
 
+__attribute__((noreturn))
 void	msg_exit(const char *str, int err_code)
 {
 	printf("%s", str);
 	exit(err_code);
 }
 
-
+__attribute__((noreturn))
 void	str_msg_exit(const char *str, const char *str_arg, int err_code)
 {
 	printf(str, str_arg);
