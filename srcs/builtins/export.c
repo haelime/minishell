d2a0a8dd1e818337ec3e3cdee5b091a0149e90d0
haelime <gnostalgiclife@gmail.com>
2023-09-21 17:41:30 +0900

    export done. needs test

--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -6,9 +6,64 @@
 /*   By: haeem <haeem@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 16:45:29 by haeem             #+#    #+#             */
-/*   Updated: 2023/09/15 17:47:29 by haeem            ###   ########seoul.kr  */
+/*   Updated: 2023/09/21 17:40:48 by haeem            ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
+static int	norm_builtin_export(t_hashmap *envmap, char *key, char *value)
+{
+	if (key == NULL)
+		return (BUILTIN);
+	if (value == NULL)
+		value = ft_strdup("");
+	hashmap_insert(envmap, key, value);
+	free(key);
+	free(value);
+	return (SUCCESS);
+}
+
+// syntax is already checked & replaced dollar, so trust every argv
+//   end
+//    |
+// zzz=asdf           get key "zzz"
+// ^
+// start
+// -------------------------------------------------------------------
+//   end
+//    |
+// zzz=asdf           move start to first found "="
+//    ^
+//  start
+// -------------------------------------------------------------------
+//        end
+//         |
+// zzz=asdf           get value "asdf"
+//    ^
+//  start
+int	builtin_export(char **argv, t_hashmap *envmap)
+{
+	int		start;
+	int		end;
+	char	*key;
+	char	*value;
+
+	if (*(argv + 1) == NULL)
+		print_hashmap(envmap);
+	while (*(++argv))
+	{
+		start = 0;
+		end = 0;
+		while ((*argv)[end] != '=' && (*argv)[end] != '\0')
+			end++;
+		if ((*argv)[end] == '\0')
+			return (SUCCESS);
+		key = ft_substr(*argv, start, end - start);
+		start = end;
+		while ((*argv)[end] != '\0')
+			end++;
+		value = ft_substr(*argv, start, end - start);
+	}
+	return (norm_builtin_export(envmap, key, value));
+}
