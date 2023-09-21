09b149b5eec300487ee1f92c7ce6119b924727e3
Hyunjun Kim <hyunjun2372@gmail.com>
2023-09-21 19:28:42 +0900

    Implementation free_parse() and free_cmd_blocks()

---
+++ srcs/parse/free_parse.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_parse.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyunjunk <hyunjunk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/21 17:55:58 by hyunjunk          #+#    #+#             */
+/*   Updated: 2023/09/21 19:27:54 by hyunjunk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+void	free_tokens(t_list **out_tokens)
+{
+	t_list	*p;
+	t_list	*next;
+	t_token	*token;
+
+	p = *out_tokens;
+	while (p != NULL)
+	{
+		token = (t_token *)p->content;
+		next = p->next;
+		free(token->str);
+		free(p->content);
+		free(p);
+		p = next;
+	}
+	*out_tokens = NULL;
+}
+
+void	free_cmd_blocks(t_list **out_cmd_blocks)
+{
+	t_list		*p;
+	t_list		*next;
+	t_cmd_block	*cmd_block;
+
+	p = *out_cmd_blocks;
+	while (p != NULL)
+	{
+		cmd_block = (t_cmd_block *)p->content;
+		next = p->next;
+		free(cmd_block->options);
+		free(cmd_block->completed_cmd);
+		free(p->content);
+		free(p);
+		p = next;
+	}
+	*out_cmd_blocks = NULL;
+}
