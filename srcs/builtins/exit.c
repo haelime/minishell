fd16dc55cc3d9b512e427a38e58820852ad5ecb2
haelime <gnostalgiclife@gmail.com>
2023-09-28 19:42:23 +0900

    exit done

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyunjunk <hyunjunk@student.42.fr>          +#+  +:+       +#+        */
+/*   By: haeem <haeem@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 16:45:27 by haeem             #+#    #+#             */
-/*   Updated: 2023/09/24 16:21:51 by hyunjunk         ###   ########.fr       */
+/*   Updated: 2023/09/28 19:42:11 by haeem            ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,18 @@
 // maybe should replace exit to return.
 int	builtin_exit(char **argv)
 {
-	int				i;
 	unsigned char	exitcode;
 
 	if (argv[1] == NULL)
 		exit(SUCCESS);
+	if (!isdigit(*argv[1]))
+		str_msg_exit("exit: %s numeric argument required\n",
+			argv[1], NOTNUMERIC);
 	else if (argv[2] != NULL)
-		exit(BUILTIN);
-	while (*(++argv))
 	{
-		i = 0;
-		while (argv[i] != NULL)
-		{
-			if (!isdigit(*argv[i]))
-				exit(NOTNUMERIC);
-			i++;
-		}
-		exitcode = ft_atoi(*argv);
-		exit(exitcode);
+		ft_putstr_fd("exit: too many arguments\n", STDERR_FILENO);
+		return (FAILURE);
 	}
-	printf("DEBUG : exit() reach un-reachable.\n"); //< DEBUG
-	exit(-1);
+	exitcode = ft_atoi(argv[1]);
+	exit(exitcode);
 }
